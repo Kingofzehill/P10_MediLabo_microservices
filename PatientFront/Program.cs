2f6e2995f408ed1283645322f753eb1ff901b363
Kingofzehill <kingofzehill@gmail.com>
2024-10-10 10:26:43 +0200

    #PatientNoteBackAPI microservice - create a APS.NET Core Web API project. - NuGet packages : -- Microsoft.AspNetCore.Authentication.JwtBearer 8.0.8 -- Microsoft.AspNetCore.Identity.EntityFrameworkCore 8.0.8 -- Microsoft.EntityFrameworkCore.SqlServer 8.0.8 -- MongoDB.EntityFrameworkCore 8.1.1 -- Serilog 4.0.1 -- Serilog.Sinks.File 6.0.0 -- Serilog.Sinks.Console 6.0.0
    #PatientNoteBackAPI NOSQL DB
    - DBName : P10_MediLabo_PatientNotes
    -- created in MongoDbShell
    -- Note table, fields : patientId and Note. ObjectId (identifier field) is automatically generated by MongoDb.
    
    #PatientFront access to PatientNoteBackAPI microservice.
    - add a project refernce to PatientNoteBackAPI.

--- PatientFront/Program.cs
+++ PatientFront/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddSession(options =>
     options.IdleTimeout = TimeSpan.FromMinutes(30);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
+    options.Cookie.Expiration = TimeSpan.FromMinutes(30);
 });
 
 builder.Services.AddHttpContextAccessor();
